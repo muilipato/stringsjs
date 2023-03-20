5dedc79fea1b6387ac1a0ec4f9c9ea16ed69ec21
Patrick Maweu <muilipato@gmail.com>
2023-03-20 10:20:50 +0300

    strings assignment

---
+++ index.js
@@ -0,0 +1,5 @@
+// Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`; 
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
